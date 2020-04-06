bf0bfd5f27bd3bfa1c088bc109182b3dbd358c71
Salifu Sani Rich <iamsarscode@gmail.com>
2020-04-06 23:44:16 +0100

    feat: Implement logic for random character generator function

--- src/js/random.js
+++ src/js/random.js
@@ -1,11 +1,17 @@
+'use strict';
+
 const { alpha, symbols } = require('./characters.json');
 
 const generateRandom = () => {
   return {
-    number: () => {},
-    lowercase: () => {},
-    uppercase: () => {},
-    symbol: () => {},
+    number: () => Math.floor(Math.random() * 10),
+    lowercase: () => alpha[Math.floor(Math.random() * alpha.length)],
+    /* Used standard funtion to point `this` to the Object returned by generateRandom */
+    uppercase: function () {
+      console.log(this);
+      return this.lowercase().toUpperCase();
+    },
+    symbol: () => symbols[Math.floor(Math.random() * symbols.length)],
   };
 };
 
