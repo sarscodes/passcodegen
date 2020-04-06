f6166a3ad84e328a0f105c474d5d08efd5adfa09
Salifu Sani Rich <iamsarscode@gmail.com>
2020-04-06 22:53:59 +0100

    feat: Add random character generator function

---
+++ src/js/random.js
@@ -0,0 +1,12 @@
+const { alpha, symbols } = require('./characters.json');
+
+const generateRandom = () => {
+  return {
+    number: () => {},
+    lowercase: () => {},
+    uppercase: () => {},
+    symbol: () => {},
+  };
+};
+
+module.exports = generateRandom();
